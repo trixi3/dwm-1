e2952008ddaa7c0040ed6c0abe6083c90016b561
trixi3 <aquinojosue3@gmail.com>
2013-11-10 14:16:39 -0600

    Update config.h

--- config.h
+++ config.h
@@ -25,7 +25,7 @@ static const char font[] = "-*-ohsnap.icons-medium-r-*-*-14-*-*-*-*-*-*-*";
 static const char colors[NUMCOLORS][ColLast][13] = {
     /* border    fg         bg */
     { "#2D2D2D", "#FFFFFF", "#2D2D2D" },        /* 01 - regular */
-    { "#D64937", "#FFFFFF", "#2D2D2D" },        /* 02 - selected */
+    { "#4A8DC3", "#FFFFFF", "#2D2D2D" },        /* 02 - selected */
     { "#2D2D2D", "#FF0000", "#2D2D2D" },        /* 03 - urgent */
     { "#2D2D2D", "#666666", "#2D2D2D" },        /* 04 - occupied */
     { "#2D2D2D", "#A82222", "#2D2D2D" },        /* 05 - red */
@@ -33,7 +33,7 @@ static const char colors[NUMCOLORS][ColLast][13] = {
     { "#2D2D2D", "#349147", "#2D2D2D" },        /* 07 - green */
     { "#2D2D2D", "#666666", "#2D2D2D" },        /* 08 - dark grey */
     { "#2D2D2D", "#DCDCDC", "#2D2D2D" },        /* 09 - light grey */
-    { "#2D2D2D", "#D64937", "#2D2D2D" },        /* 0A - orange */
+    { "#2D2D2D", "#4A8DC3", "#2D2D2D" },        /* 0A - orange */
     { "#2D2D2D", "#B86A6A", "#2D2D2D" },        /* 0B - pink */
     { "#2D2D2D", "#FFFFFF", "#2D2D2D" },        /* 0C - white */
     { "#2D2D2D", "#000000", "#2D2D2D" },        /* 0D - black */
@@ -41,17 +41,17 @@ static const char colors[NUMCOLORS][ColLast][13] = {
 
 static const Layout layouts[] = {
     /* symbol   gaps    arrange */
-    { "þ",      True,   tile },
-    { "ü",      True,   bstack },
-    { "ÿ",      False,  monocle },
-    { "ý",      False,  NULL },
+    { "t",      True,   tile },
+    { "b",      True,   bstack },
+    { "m",      False,  monocle },
+    { "N",      False,  NULL },
 };
 
 static const Tag tags[] = {
     /* name     layout          mfact   nmaster */
     { "web",    &layouts[0],    -1,     -1 },
     { "chat",   &layouts[0],    -1,     -1 },
-    { "term",   &layouts[3],    -1,     -1 },
+    { "term",   &layouts[0],    -1,     -1 },
     { "code",   &layouts[0],    -1,     -1 },
     { "media",  &layouts[0],    -1,     -1 },
     { "misc",   &layouts[0],    -1,     -1 },
@@ -59,47 +59,30 @@ static const Tag tags[] = {
 
 static const Rule rules[] = {
     /* class                instance    title       tags mask       isfloating      iscentred       monitor */
-    { "Google-chrome",      NULL,       NULL,       1 << 0,         False,          False,          -1 },
+    { "firefox",      NULL,       NULL,       1 << 0,         False,          False,          -1 },
     { "Skype",              NULL,       NULL,       1 << 1,         False,          False,          -1 },
-    { "URxvt",              NULL,       NULL,       1 << 2,         True,           True,           -1 },
-    { "Geany",              NULL,       NULL,       1 << 3,         False,          False,          -1 },
+    { "URxvt",              NULL,       NULL,       1 << 2,         False,          False,          -1 },
     { "Vlc",                NULL,       NULL,       1 << 4,         False,          False,          -1 },
-    { "Pcmanfm",            NULL,       NULL,       1 << 5,         False,          False,          -1 },
+    { "Geany",                NULL,       NULL,       1 << 3,         False,          False,          -1 },
+    { "Pcmanfm",            NULL,       NULL,       NULL,         False,          False,          -1 },
     { "Transmission-gtk",   NULL,       NULL,       1 << 5,         False,          False,          -1 },
 };
 
-static const char *menu[] = { "dmenu_run", "-i", "-fn", font, "-nb", colors[0][ColBG], "-nf", colors[0][ColFG], "-sb", colors[1][ColBG], "-sf", colors[9][ColFG], NULL };
-static const char *webb[] = { "google-chrome", NULL, "Google-chrome" };
+static const char *menu[] = { "dmenu_run", "-i", "-fn", font, "-nb", colors[0][ColBG], "-nf", colors[0][ColFG], "-sb", colors[1][ColBG], "-sf", colors[1][ColFG], NULL };
+static const char *webb[] = { "firefox", NULL, "firefox" };
 static const char *chat[] = { "skype", NULL, "Skype" };
-static const char *term[] = { "urxvtc", NULL, "URxvt" };
-static const char *edit[] = { "geany", NULL, "Geany" };
+static const char *term[] = { "urxvt", NULL, "URxvt" };
+static const char *edit[] = { "leafad", NULL, "Geany" };
 static const char *mdia[] = { "vlc", NULL, "Vlc" };
-static const char *file[] = { "pcmanfm", NULL, "Pcmanfm" };
 static const char *prts[] = { "scrot", NULL };
-static const char *play[] = { "ncmpcpp", "toggle" };
-static const char *stop[] = { "ncmpcpp", "stop" };
-static const char *prev[] = { "ncmpcpp", "prev" };
-static const char *next[] = { "ncmpcpp", "next" };
-static const char *mute[] = { "amixer", "-q", "set", "PCM", "toggle", NULL };
-static const char *volu[] = { "amixer", "-q", "set", "PCM", "5%+", "unmute", NULL };
-static const char *vold[] = { "amixer", "-q", "set", "PCM", "5%-", "unmute", NULL };
-
 static Key keys[] = {
-    { MONKEY,           XK_p,       spawn,          {.v = menu } },
+    { MODKEY,           XK_p,       spawn,          {.v = menu } },
     { MONKEY,           XK_w,       runorraise,     {.v = webb } },
     { MONKEY,           XK_s,       runorraise,     {.v = chat } },
     { MONKEY,           XK_Return,  runorraise,     {.v = term } },
     { MONKEY,           XK_e,       runorraise,     {.v = edit } },
     { MONKEY,           XK_m,       runorraise,     {.v = mdia } },
-    { MONKEY,           XK_f,       runorraise,     {.v = file } },
     { MONKEY,           XK_Print,   spawn,          {.v = prts } },
-    { MONKEY,           XK_F5,      spawn,          {.v = play } },
-    { MONKEY,           XK_F6,      spawn,          {.v = stop } },
-    { MONKEY,           XK_F7,      spawn,          {.v = prev } },
-    { MONKEY,           XK_F8,      spawn,          {.v = next } },
-    { MONKEY,           XK_F10,     spawn,          {.v = mute } },
-    { MONKEY,           XK_F11,     spawn,          {.v = vold } },
-    { MONKEY,           XK_F12,     spawn,          {.v = volu } },
     { MODKEY|ShiftMask, XK_q,       quit,           {0} },
     { MODKEY|ShiftMask, XK_b,       togglebar,      {0} },
     { MODKEY|ShiftMask, XK_c,       killclient,     {0} },
@@ -116,7 +99,7 @@ static Key keys[] = {
     { MODKEY,           XK_h,       setmfact,       {.f = -0.05 } },
     { MODKEY,           XK_l,       setmfact,       {.f = +0.05 } },
     { MODKEY,           XK_equal,   incnmaster,     {.i = +1 } },
-    { MODKEY,           XK_minus,   incnmaster,     {.i = -1 } },
+    { MODKEY,           XK_equal,   incnmaster,     {.i = +1 } },
     { MODKEY,           XK_Down,    focusstack,     {.i = +1 } },
     { MODKEY,           XK_Up,      focusstack,     {.i = -1 } },
     { MODKEY,           XK_0,       view,           {.ui = ~0 } },
